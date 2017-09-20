f4c7b3217eb21e5b88b17047714371b8717b739e
songyan <yansong@Yans-MacBook-Pro.local>
2017-09-19 21:34:28 -0400

    clean files after a short test

---
+++ assignment1/src/generateIndex.java
@@ -0,0 +1,5 @@
+/**
+ * Created by yansong on 9/19/17.
+ */
+public class generateIndex {
+}
---
+++ assignment1/src/helloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * Created by yansong on 9/19/17.
+ */
+public class helloWorld {
+
+    public static void main (String[] args){
+        System.out.println("hello world");
+    }
+}
